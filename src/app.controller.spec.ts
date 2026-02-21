6d25264f7f60c42f6065f88c9285192a8b3f5486
micky <mickyfads5@gmail.com>
2026-02-21 17:29:34 -0400

    fix: test update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Bienvenido a NestJs"', () => {
+      expect(appController.getHello()).toBe('Bienvenido a NestJs');
     });
   });
 });
